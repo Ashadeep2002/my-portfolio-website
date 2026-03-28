e3a13aebc380eb20c770c0f061c4b7c5efbb1c01
Ashadeep Pan <ashadeeppan18@gmail.com>
2026-03-29 03:54:45 +0530

    fix cv path

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -68,7 +68,7 @@ const Hero = () => {
 
           <a
             className="btn btn-secondary"
-            href= "/Ashadeep_Pan_Resume.pdf"
+            href={process.env.PUBLIC_URL + "/Ashadeep_Pan_Resume.pdf"}
             download="Ashadeep_Pan_Resume.pdf"
           >
             📄 Download CV
